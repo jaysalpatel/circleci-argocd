2854ee43a2bc46255211a27e1ff29f14e833fcee
jp <sample>
2020-09-18 20:23:36 -0500

    update1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>hello world</h1>
-    </div>
-  );
-}
 
-export default App;
+class App extends React.Component{
+  render() {
+    return(
+      <div>
+      <h3>hi</h3>
+      </div>
+    )
+  }
+}
